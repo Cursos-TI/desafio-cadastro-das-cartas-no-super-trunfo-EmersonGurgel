54ee15691811741ffd3b5d955cc3d8ab8ae9ef80
EmersonGurgel <202502613768@alunos.estacio.br>
2025-02-18 14:52:39 +0000

    Sistema das cartas A02 e B01 foram criados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,8 +18,10 @@ int main(){
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
- 
+//INICIO DA CARTA A01 (ESTADO A);
 // Definição de variáveis do (estado-carta A01);
+// A PARTIR DA CARTA A02 FOI USADO (VARIAVEL+ IDENTIFICAÇÃO DA CARTA);
+// EX: "ESTADO+a02), ESTADO+A02, ESTADO+B01, ESTADO+B02...;
 char estado;
 char codigo[20];
 char nomedacidade[20];
@@ -29,7 +31,8 @@ float pib;
 int pontosturisticos;
 
 //titulo de apresentação;
-printf("Bem vindo ao Desafio Super Trunfo\n\n");
+printf("                                                     Bem vindo ao Desafio Super Trunfo.\n\n\n");
+printf("                                                                CARTA A01\n\n");
 
 // dizendo ao usuario as informações que serão coletadas e coletando elas;
 printf("Digite o estado:\n");
@@ -55,6 +58,7 @@ scanf("%d", &pontosturisticos);
 printf("\n\n");
 
 //Nessa parte abaixo será imprimido os dados que foram coletados do usuario;
+printf("Dados da carta A01:\n");
 printf("Estado: %c\n", estado);
 printf("Código da cidade: %s\n", codigo);
 printf("Nome da cidade: %s\n", nomedacidade);
@@ -62,12 +66,13 @@ printf("População: %d\n", populacao);
 printf("Área: %f km²\n", area);
 printf("Pib: %f\n", pib);
 printf("Pontos turísticos: %d\n", pontosturisticos);
-printf("\n\n\n");
+printf("\n\n");
 
 //inicio da carta A02 (estado A);
 
 // Variaveis da carta A02 (estado A);
 //foi adicionado "a02" a frente das variaveis para facilitar a identificacão;
+//EX: ESTADO+a02;
 char estadoa02;
 char codigoa02[20];
 char nomedacidadea02[20];
@@ -78,7 +83,7 @@ int pontosturisticosa02;
 
 //introdução de titúlo da carta A02;
 printf("Carta A02\n\n");
-printf("Nos campos abaixo informe as informações da carta A02\n\n");
+printf("Nos campos abaixo informe as informações da carta A02.\n\n");
 
 // dizendo ao usuario as informações que serão coletadas e coletando elas
 printf("Digite o estado:\n");
@@ -104,6 +109,7 @@ scanf("%d", &pontosturisticosa02);
 printf("\n\n\n");
 
 //Nessa parte abaixo será imprimido os dados que foram coletados do usuario;
+printf("Dados da carta A02:\n");
 printf("Estado: %c\n", estadoa02);
 printf("Código da cidade: %s\n", codigoa02);
 printf("Nome da cidade: %s\n", nomedacidadea02);
@@ -111,6 +117,64 @@ printf("População: %d\n", populacaoa02);
 printf("Área: %f km²\n", areaa02);
 printf("Pib: %f\n", piba02);
 printf("Pontos turísticos: %d\n", pontosturisticosa02);
+printf("\n\n");
+
+//TÍTULO DA CARTA BO1
+printf("Carta B01\n\n");
+
+// INÍCIO DO ESTADO B;
+//INÍCIO DA CARTA B01
+
+//DEFININDO AS VARIÁVEIS DA CARTA B01 (ESTADO B);
+// FOI USADO "B01" AO FINAL DO NOME DA VARÍAVEL PARA FACILITAR A IDENTIFICAÇÃO;
+// EX: ESTADO+b01;
+char estadob01;
+char codigob01[20];
+char nomedacidadeb01[20];
+int populacaob01;
+float areab01;
+float pibb01;
+int pontosturisticosb01;
+
+// SERÁ FEITA A SOLICITACÃO E A COLETA DOS DADOS DAS CARTAS AO USUÁRIO;
+printf("Digite o estado:\n");
+scanf(" %c", &estadob01);
+
+printf("Digite o código:\n");
+scanf(" %s", codigob01);
+
+printf("Digite o nome da cidade:\n");
+scanf(" %20[^\n]", nomedacidadeb01);
+
+printf("Digite a população:\n");
+scanf("%d", &populacaob01);
+
+printf("Digite a área:\n");
+scanf("%f", &areab01);
+
+printf("Digite o pib:\n");
+scanf("%f", &pibb01);
+
+printf("Digite o número de pontos turísticos:\n");
+scanf("%d", &pontosturisticosb01);
+printf("\n\n");
+
+//Nessa parte abaixo será imprimido os dados que foram coletados do usuario;
+printf("Dados da carta B01:\n");
+printf("Estado: %c\n", estadob01);
+printf("Código da cidade: %s\n", codigob01);
+printf("Nome da cidade: %s\n", nomedacidadeb01);
+printf("População: %d\n", populacaob01);
+printf("Área: %f km²\n", areab01);
+printf("Pib: %f\n", pibb01);
+printf("Pontos turísticos: %d\n", pontosturisticosb01);
+printf("\n\n\n");
+
+//INICIO DA CARTA B02 (PERTENCE AO ESTADO B);
+//DEFININDO AS VARIÁVEIS DA CARTA B02 (ESTADO B);
+// FOI USADO "B02" AO FINAL DO NOME DA VARÍAVEL PARA FACILITAR A IDENTIFICAÇÃO;
+// EX: ESTADO+b02;
+
 
 
 
